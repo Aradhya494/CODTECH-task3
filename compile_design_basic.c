3d9be9394e50453ff1ceb7d0bd2c13df5a071c53
Aradhya494 <rajhansaradhya21@gmail.com>
2025-06-20 21:11:36 +0530

    Update compile_design_basic.c

--- compile_design_basic.c
+++ compile_design_basic.c
@@ -3,66 +3,61 @@
 #include <string.h>
 #include <ctype.h>
 
-
 const char *keywords[] = {
-    "int", "float", "char", "double", "long", "short",
-    "if", "else", "while", "for", "do", "return",
-    "switch", "case", "break", "continue", "void",
-    "static", "struct", "sizeof", "typedef", "union"
+    "int", "float", "char", "double", "if", "else", "while", "for", "return", 
+    "void", "main", "switch", "case", "break", "continue", "static", "struct"
 };
 
-
-int isKeyword(const char *str) {
-    for (int i = 0; i < sizeof(keywords)/sizeof(keywords[0]); i++) {
-        if (strcmp(str, keywords[i]) == 0)
+int isKeyword(const char *word) {
+    for (int i = 0; i < sizeof(keywords) / sizeof(keywords[0]); i++) {
+        if (strcmp(word, keywords[i]) == 0)
             return 1;
     }
     return 0;
 }
 
 int isOperator(char ch) {
-    return (ch == '+' || ch == '-' || ch == '*' || ch == '/' ||
-            ch == '=' || ch == '<' || ch == '>' || ch == '%' ||
-            ch == '&' || ch == '|' || ch == '!');
-}
-
-
-void lexicalAnalyzer(FILE *file) {
-    char ch, buffer[100];
-    int i = 0;
-
-    while ((ch = fgetc(file)) != EOF) {
-    
-        if (isalnum(ch) || ch == '_') {
-            buffer[i++] = ch;
-        } else {
-            if (i != 0) {
-                buffer[i] = '\0';
-                if (isKeyword(buffer)) {
-                    printf("Keyword: %s\n", buffer);
-                } else {
-                    printf("Identifier: %s\n", buffer);
-                }
-                i = 0;
-            }
-
-
-            if (isOperator(ch)) {
-                printf("Operator: %c\n", ch);
-            }
-        }
-    }
+    return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '=' || ch == '>';
 }
 
 int main() {
     FILE *file = fopen("input.c", "r");
     if (!file) {
-        printf("Error: Could not open file.\n");
+        printf("Failed to open input file.\n");
         return 1;
     }
 
-    printf("Lexical Analysis Output:\n\n");
-    lexicalAnalyzer(file);
+    char ch, buffer[100];
+    int i = 0;
+
+    printf("Lexical Analysis Output:\n");
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (isalpha(ch)) {
+            buffer[i++] = ch;
+        } else if (isdigit(ch)) {
+            buffer[i++] = ch;
+        } else if (ch == ' ' || ch == '\n' || ch == ';' || ch == '(' || ch == ')' || ch == '{' || ch == '}' || ch == ',') {
+            if (i > 0) {
+                buffer[i] = '\0';
+                if (isKeyword(buffer))
+                    printf("[Keyword]: %s\n", buffer);
+                else
+                    printf("[Identifier]: %s\n", buffer);
+                i = 0;
+            }
+        } else if (isOperator(ch)) {
+            if (i > 0) {
+                buffer[i] = '\0';
+                if (isKeyword(buffer))
+                    printf("[Keyword]: %s\n", buffer);
+                else
+                    printf("[Identifier]: %s\n", buffer);
+                i = 0;
+            }
+            printf("[Operator]: %c\n", ch);
+        }
+    }
 
     fclose(file);
     return 0;
